d0a0bf4019eeeae91f9712effa753e1c00b8b7d9
Cole Willsea <coleww@gmail.com>
2015-12-04 06:25:19 -0800

    clearly

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = function (gainNode, when, adsr) {
   gainNode.gain.exponentialRampToValueAtTime(adsr.peak, when + adsr.attack)
   gainNode.gain.exponentialRampToValueAtTime(adsr.mid, when + adsr.attack + adsr.decay)
   gainNode.gain.setValueAtTime(adsr.mid, when + adsr.sustain + adsr.attack + adsr.decay)
-  gainNode.gain.exponentialRampToValueAtTime(0, when + adsr.release)
+  gainNode.gain.exponentialRampToValueAtTime(0, when + adsr.sustain + adsr.attack + adsr.decay + adsr.release)
 }
