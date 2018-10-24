4841ef56ed8ae4c07be7f2b3862e842ef73a4194
Cole Willsea <coleww@gmail.com>
2018-10-23 18:07:02 -0700

    throw good errors if passed bad args

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 module.exports = function (gainNode, when, adsr) {
+  if (!gainNode) throw `you must pass a gain node to me, got ${gainNode}`
+  if (typeof when !== 'number') throw `you must pass "when" to me, got ${when}`
+  ;['peak', 'mid', 'end', 'attack', 'sustain', 'release', 'decay'].forEach(function (attr) {
+    if (typeof adsr[attr] !== 'number') throw `you must tell me the ${attr}, got ${adsr[attr]}`
+  })
+  
   gainNode.gain.exponentialRampToValueAtTime(adsr.peak, when + adsr.attack)
   gainNode.gain.exponentialRampToValueAtTime(adsr.mid, when + adsr.attack + adsr.decay)
   gainNode.gain.setValueAtTime(adsr.mid, when + adsr.sustain + adsr.attack + adsr.decay)
--- test.js
+++ test.js
@@ -1,27 +1,28 @@
 var tap = require('tape')
 require('web-audio-mock')
-var AudioContext = AudioContext || webkitAudioContext
 var ac = new AudioContext()
 var adsr = require('./')
 var gain = ac.createGain()
-gain.gain.setValueAtTime(0, ac.currentTime)
 
-tap.test('does the thing', function (t) {
-  t.plan(1)
-  t.equal(gain.gain.value, 0)
-  adsr(gain, ac.currentTime, {attack: 0.2, decay: 0.2, sustain: 0.2, release: 0.2, peak: 1, mid: 0.5, end: 0.0001})
-  // i guess web audio mock doesn't do linearRamps or something?? :<
-  // could test in browser i guess...
-  // setTimeout(function () {
-  //   t.equal(gain.gain.value, 1)
-  //   setTimeout(function () {
-  //     t.equal(gain.gain.value, 0.5)
-  //     setTimeout(function () {
-  //       t.equal(gain.gain.value, 0.5)
-  //       setTimeout(function () {
-  //         t.equal(gain.gain.value, 0)
-  //       }, 210)
-  //     }, 210)
-  //   }, 210)
-  // }, 210)
+tap.test('throws errors if passed bad args', function (t) {
+  t.plan(4)
+  t.throws(
+    adsr.bind(null, undefined, ac.currentTime, {attack: 0.2, decay: 0.2, sustain: 0.2, release: 0.2, peak: 1, mid: 0.5, end: 0.0001}),
+    'you must pass a gain node to me, got undefined'
+  )
+
+  t.throws(
+    adsr.bind(null, gain, 'thirty', {attack: 0.2, decay: 0.2, sustain: 0.2, release: 0.2, peak: 1, mid: 0.5, end: 0.0001}),
+    'you must pass "when" to me, got thirty'
+  )
+
+  t.throws(
+    adsr.bind(null, gain, undefined, {attack: 'foo', decay: 0.2, sustain: 0.2, release: 0.2, peak: 1, mid: 0.5, end: 0.0001}),
+    'you must tell me the attack, got foo'
+  )
+
+  t.throws(
+    adsr.bind(null, gain, undefined, {attack: 0, decay: undefined, sustain: 0.2, release: 0.2, peak: 1, mid: 0.5, end: 0.0001}),
+    'you must tell me the decay, got undefined'
+  )
 })
